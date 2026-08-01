f549c08a383dba661feb2ffb65f37717330493bf
ELDA7EE7 <mazenmohamedabd@gmail.com>
2026-08-01 20:58:21 +0300

    optimized authentication tokens by storing the user id and using it to identify the user instead of the email

--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.services;
 
+import com.codewithmosh.store.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -13,10 +14,12 @@ import java.util.Date;
 public class JwtService {
     @Value("${spring.jwt.secret}")
     private String secret;
-    public String generateToken(String email) {
+    public String generateToken(User user) {
         final long tokenExpiration = 86400; // 1 day
         return Jwts.builder()
-                .subject(email)
+                .subject(String.valueOf(user.getId()))
+                .claim("email", user.getEmail())
+                .claim("name", user.getName())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
@@ -41,7 +44,7 @@ public class JwtService {
             .getPayload();
     }
 
-    public String getEmailFromToken(String token){
-         return getClaims(token).getSubject();
+    public Long getUserIdFromToken(String token){
+         return Long.valueOf(getClaims(token).getSubject());
     }
 }
