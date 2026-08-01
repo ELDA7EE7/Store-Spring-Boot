50075db79ab33d6823bb97b61286c778e4f8c376
ELDA7EE7 <mazenmohamedabd@gmail.com>
2026-08-01 21:41:19 +0300

    feat: implemented the refresh api endpoint

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -56,11 +56,20 @@ public class AuthController {
     public ResponseEntity<Void> handleBadCredentialsException(){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
-    @PostMapping("/validate")
-    public boolean validate(@RequestHeader("Authorization") String authHeader){
-        System.out.println("Validate called");
-        var token = authHeader.replace("Bearer ",""); // "Bearer "
-        return jwtService.validateToken(token);
+
+    @PostMapping ("/refresh")
+    public ResponseEntity<JwtResponse> refresh(
+            @CookieValue(value = "refreshToken") String refreshToken
+    ){
+        if(!jwtService.validateToken(refreshToken)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        var userId = jwtService.getUserIdFromToken(refreshToken);
+        var user = userRepository.findById(userId).orElseThrow();
+        var accessToken = jwtService.generateAccessToken(user);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
     @GetMapping("/me")
