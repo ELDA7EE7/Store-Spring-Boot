1137913d69bcabc4e922d258f3c8b038cbbd2a30
ELDA7EE7 <mazenmohamedabd@gmail.com>
2026-08-08 18:26:32 +0300

    created a checkout session

--- src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
@@ -5,7 +5,9 @@ import lombok.Data;
 @Data
 public class CheckoutResponse {
     private Long orderId;
-    public CheckoutResponse(Long orderId) {
+    private String checkoutUrl;
+    public CheckoutResponse(Long orderId, String checkoutUrl) {
         this.orderId = orderId;
+        this.checkoutUrl = checkoutUrl;
     }
 }
